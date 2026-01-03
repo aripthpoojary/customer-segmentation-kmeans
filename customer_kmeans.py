d17ef2df0a0e11855932e013259cf750a9742f17
aripthpoojary <aripth.poojary@gmail.com>
2026-01-03 12:13:37 +0530

    Create customer_kmeans.py

---
+++ customer_kmeans.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# Load dataset
+data = pd.read_csv("dataset.csv")
+
+# Select features
+X = data[['Annual Income', 'Spending Score']]
+
+# Elbow Method
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), wcss)
+plt.xlabel("Number of Clusters")
+plt.ylabel("WCSS")
+plt.title("Elbow Method")
+plt.show()
+
+# Apply K-Means
+kmeans = KMeans(n_clusters=5, random_state=42)
+data['Cluster'] = kmeans.fit_predict(X)
+
+# Visualize clusters
+plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=data['Cluster'])
+plt.xlabel("Annual Income")
+plt.ylabel("Spending Score")
+plt.title("Customer Segmentation")
+plt.show()
